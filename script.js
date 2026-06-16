7fb2ff13ca2e1cfc86fb70362ac7fa16ffe840ca
monicaazaria <monicamazaria@gmail.com>
2026-06-16 21:40:15 +0200

    Add dark mode toggle

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark");
+}
